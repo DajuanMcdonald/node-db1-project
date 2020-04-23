be2ff092304af8a2527bd1ac4f153e131605a228
Dajuan Mcdonald <flopwiki@gmail.com>
2020-04-22 21:12:35 -0400

    fixed db import

--- routes/accountsRoute.js
+++ routes/accountsRoute.js
@@ -1,11 +1,33 @@
 const express = require('express');
-const db = require('../data/seeds/accounts');
+const db = require('../data/dbConfig');
+const accounts = require('../data/seeds/accounts')
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
+router.get('/', (req, res) => {
+    db.select('*').from('accounts')
+    .then( acc => {
+        res.status(200).jsonp({data: acc})
+    }).catch(err => {
+        res.status(500).json({message: "Server Error"})
+    })
+     
+    
+
+})
+
+router.get('/:id', async (req, res, err, next) => {
+    try {
+
+    }
+    catch {
+        err 
+        res.status(500).json({message: "Server error"})
+        
+    }
+    const [id] = req.params;
     // const path = await path(__filename, 'index.html')
-    res.status(200).send('<h1>Introduction to Relational Databases</h1>')
+    res.status(200).json(`<h1>Introduction to Relational Databases</h1>, ${id}`)
 
 })
 
