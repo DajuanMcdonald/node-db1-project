a5a1b6b61c36a890b000925208f350578c349cc2
Dajuan Mcdonald <flopwiki@gmail.com>
2020-04-22 22:42:09 -0400

    added delete request

--- routes/accountsRoute.js
+++ routes/accountsRoute.js
@@ -36,13 +36,36 @@ router.get('/', (req, res) => {
 
 //find by id :  READ
 router.get('/:id', (req, res) => {
-        db('accounts').where({id : req.params.id})
-        .then( item => {
-            res.status(200).jsonp({account: item})
+        db('accounts').where({id : req.params.id}).first()
+        .then( acc => {
+            if(acc) {
+            res.status(200).jsonp({account: acc})
+            } else {
+                res.status(404).json({message: "No such account in database with that Id"})
+            }
         })
         .catch(err => {
             res.status(500).json({message: 'Server error'})
         })
+
+})
+
+//remove account from db : REMOVE/DELETE 
+router.delete('/:id', (req, res) => {
+    db('accounts').where({id: req.params.id}).del()
+    .then( count => {
+        if (count > 0) {
+
+            res.status(200).json({message: 'Record deleted Successfully.'})
+        } else {
+            res.status(404).json({message: "Account not found"})
+        }
+    }).catch(err => {
+        res.status(500).json({message: "There was a server error"})
+    })
+})
+
+router.put('/:id', (req, res) => {
 
 })
 
