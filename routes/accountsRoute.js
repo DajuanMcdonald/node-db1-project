1db7a820a3af2596352f4297a12cfeef8f9b10d2
Dajuan Mcdonald <flopwiki@gmail.com>
2020-04-22 21:26:03 -0400

    fixed getById and getAll

--- routes/accountsRoute.js
+++ routes/accountsRoute.js
@@ -4,10 +4,11 @@ const accounts = require('../data/seeds/accounts')
 
 const router = express.Router();
 
+//find all accounts in db 
 router.get('/', (req, res) => {
     db.select('*').from('accounts')
     .then( acc => {
-        res.status(200).jsonp({data: acc})
+        res.status(200).jsonp({account: acc})
     }).catch(err => {
         res.status(500).json({message: "Server Error"})
     })
@@ -16,18 +17,18 @@ router.get('/', (req, res) => {
 
 })
 
-router.get('/:id', async (req, res, err, next) => {
-    try {
 
-    }
-    catch {
-        err 
-        res.status(500).json({message: "Server error"})
-        
-    }
-    const [id] = req.params;
-    // const path = await path(__filename, 'index.html')
-    res.status(200).json(`<h1>Introduction to Relational Databases</h1>, ${id}`)
+//find by id : 
+router.get('/:id', (req, res) => {
+        db('accounts').where({id : req.params.id})
+        .then( item => {
+            res.status(200).jsonp({account: item})
+        })
+        .catch(err => {
+            res.status(500).json({message: 'Server error'})
+        })
+
+
 
 })
 
